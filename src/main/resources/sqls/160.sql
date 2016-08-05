c4f7cf00e9936e265046729bfe48d542541a2655
luoqi14 <luoqi_14@126.com>
2016-08-05 12:10:15 +0800

    1.6.0

---
+++ src/main/resources/sqls/160.sql
@@ -0,0 +1,4 @@
+/* second */
+DELETE FROM tsys_menu WHERE url = '/general/dict/add'; /*数据字典新增*/
+DELETE FROM tsys_menu WHERE url = '/general/dict/drop'; /*数据字典删除*/
+DELETE FROM tsys_menu WHERE url = '/siteImport'; /*现场发货*/
---
+++ src/main/webapp/resources/customer/js/huoyuan-replaceadd.js
@@ -0,0 +1,74 @@
+
+$(function() {
+	$('#idKind').renderDropdown(Dict.getIDKindName());
+
+	//提交
+	$('#subBtn').click(function() {
+	    if(!$("#jsForm").valid()){
+			return false;
+		}
+		var data = {};
+		var t = $('form').serializeArray();
+		$.each(t, function() {
+			data[this.name] = this.value;
+		});
+		if(pdfImg.constructor === String) data["pdf"]=pdfImg;
+		var url = $("#basePath").val()+"/product/producer/add";
+		doPostAjax(url, data, doSaveSuccessBack);
+	});
+	
+	//返回
+	$('#backBtn').click(function() {
+		location.href = $("#basePath").val()+"/customer/score2.htm";
+	});
+	
+	//入参合法性校验
+	$("#jsForm").validate({
+		rules: {
+			loginName: {
+				required: true,
+				maxlength: 11
+			},
+			mobile: {
+				required: true,
+				maxlength: 11
+			},
+			idKind: {
+				required: true,
+				maxlength: 2
+			},
+			idNo: {
+				required: true,
+				maxlength: 32
+			},
+			realName: {
+				required: true,
+				maxlength: 16
+			}
+		}
+	});
+});
+
+function selectImagepdf(file){
+	if(!file.files || !file.files[0]){
+		return;
+	}
+	var reader = new FileReader();
+	reader.onload = function(evt){
+		document.getElementById('pdf').src = evt.target.result;
+		pdfImg = evt.target.result;
+		$("#pdfImg").show();
+		$("#pdfImg").attr("src",pdfImg);
+	}
+	reader.readAsDataURL(file.files[0]);
+}
+
+
+function doSaveSuccessBack(res) {
+	if (res.success == true) {
+		alert("操作成功");
+		window.location.href = $("#basePath").val()+"/customer/score2.htm";
+	}else{
+		alert(res.msg);
+	}
+}
