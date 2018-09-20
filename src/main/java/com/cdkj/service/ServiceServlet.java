dc0825ef9ded978c34824f4449ec8b59deb75e95
haiqingzheng <leo.zheng@hichengdai.com>
2018-09-20 13:33:00 +0800

    tha v191 接口自动添加IP入参

--- src/main/java/com/cdkj/service/ServiceServlet.java
+++ src/main/java/com/cdkj/service/ServiceServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
 
 import com.cdkj.service.proxy.DispatcherImpl;
@@ -32,7 +33,7 @@ public class ServiceServlet extends HttpServlet {
             HttpServletResponse response) throws ServletException, IOException {
         logger.info("Successful Deployment");
         PrintWriter writer = response.getWriter();
-        writer.append("Version:1.6.0 \n");
+        writer.append("Version:1.9.1 \n");
         writer.append("Description:forward-service THA 1st \n");
         writer.flush();
     }
@@ -44,9 +45,43 @@ public class ServiceServlet extends HttpServlet {
             HttpServletResponse response) throws ServletException, IOException {
         String code = request.getParameter("code");
         String json = request.getParameter("json");
+        String ip = getIpAddr(request);
+        // 返回token添加给前端
+        json = json.substring(0, json.lastIndexOf("}")) + ", \"ip\":\"" + ip
+                + "\"}";
         String result = dispatcher.doDispatcher(code, json);
         PrintWriter writer = response.getWriter();
         writer.append(result);
         writer.flush();
+    }
+
+    /**
+     * 获取访问者IP
+     * 
+     * 在一般情况下使用Request.getRemoteAddr()即可，但是经过nginx等反向代理软件后，这个方法会失效。
+     * 
+     * 本方法先从Header中获取X-Real-IP，如果不存在再从X-Forwarded-For获得第一个IP(用,分割)，
+     * 如果还不存在则调用Request .getRemoteAddr()。
+     * 
+     * @param request
+     * @return
+     */
+    private String getIpAddr(HttpServletRequest request) {
+        String ip = request.getHeader("X-Real-IP");
+        if (!StringUtils.isBlank(ip) && !"unknown".equalsIgnoreCase(ip)) {
+            return ip;
+        }
+        ip = request.getHeader("X-Forwarded-For");
+        if (!StringUtils.isBlank(ip) && !"unknown".equalsIgnoreCase(ip)) {
+            // 多次反向代理后会有多个IP值，第一个为真实IP。
+            int index = ip.indexOf(',');
+            if (index != -1) {
+                return ip.substring(0, index);
+            } else {
+                return ip;
+            }
+        } else {
+            return request.getRemoteAddr();
+        }
     }
 }
