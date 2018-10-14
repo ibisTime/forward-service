b8972e164a41c142f09ba08093098852ffa61e61
haiqingzheng <leo.zheng@hichengdai.com>
2018-10-14 14:13:46 +0800

    版本信息更新

--- src/main/java/com/cdkj/service/ServiceServlet.java
+++ src/main/java/com/cdkj/service/ServiceServlet.java
@@ -33,7 +33,7 @@ public class ServiceServlet extends HttpServlet {
             HttpServletResponse response) throws ServletException, IOException {
         logger.info("Successful Deployment");
         PrintWriter writer = response.getWriter();
-        writer.append("Version:1.9.1 \n");
+        writer.append("Version:1.9.3 \n");
         writer.append("Description:forward-service THA 1st \n");
         writer.flush();
     }
